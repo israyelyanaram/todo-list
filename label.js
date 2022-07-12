60cc98022d676c4bb7b65221b511ab03c9cf4df6
israyelyanaram <garpia6aram@gmail.com>
2022-07-13 00:04:02 +0400

    modified HTML

--- label.js
+++ label.js
@@ -1,3 +1,52 @@
+const bodyStyle = document.querySelector('.body-style');
+const row = document.createElement("div");
+row.classList.add("row");
+bodyStyle.appendChild(row);
+
+let nothing1 = "";
+for(let i = 0; i < 2; i++){
+  nothing1 = nothing1 + `
+  <div>
+  <i class="fa fa-user-circle" aria-hidden="true"></i>
+  <i class="fa fa-book" aria-hidden="true"></i>
+  <i class="fa fa-bug" aria-hidden="true"></i>
+  <i class="fa fa-code" aria-hidden="true"></i>
+  <i class="fa fa-database" aria-hidden="true"></i>
+  <i class="fa fa-desktop" aria-hidden="true"></i>
+  <i class="fa fa-diamond" aria-hidden="true"></i>
+  <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+  <i class="fa fa-hashtag" aria-hidden="true"></i>
+  <i class="fa fa-star" aria-hidden="true"></i>
+  <i class="fa fa-font" aria-hidden="true"></i>
+  <i class="fa fa-bell" aria-hidden="true"></i>
+  <i class="fa fa-bookmark" aria-hidden="true"></i>
+  <i class="fa fa-heart" aria-hidden="true"></i>
+  <i class="fa fa-tasks" aria-hidden="true"></i>
+  <i class="fa fa-user-circle" aria-hidden="true"></i>
+  <i class="fa fa-book" aria-hidden="true"></i>
+  <i class="fa fa-bug" aria-hidden="true"></i>
+  <i class="fa fa-code" aria-hidden="true"></i>
+  <i class="fa fa-database" aria-hidden="true"></i>
+  <i class="fa fa-desktop" aria-hidden="true"></i>
+  <i class="fa fa-diamond" aria-hidden="true"></i>
+  <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+  <i class="fa fa-hashtag" aria-hidden="true"></i>
+  <i class="fa fa-star" aria-hidden="true"></i>
+  <i class="fa fa-font" aria-hidden="true"></i>
+  <i class="fa fa-bell" aria-hidden="true"></i>
+  <i class="fa fa-bookmark" aria-hidden="true"></i>
+  <i class="fa fa-heart" aria-hidden="true"></i>
+  <i class="fa fa-tasks" aria-hidden="true"></i>
+  </div>`;
+  row.innerHTML = `<div class="row">` + nothing1 + `</div>`;
+}
+
+let nothing2 = "";
+for(let i = 0;i < 30; i++){
+  nothing2 = nothing2 + row.innerHTML;
+  bodyStyle.innerHTML = nothing2;
+}
+
 setInterval(function () {
   const header = document.getElementById("header");
   header.innerHTML = new Date().toLocaleString();
@@ -19,6 +68,8 @@ if (isDarkMode) {
   toggle.checked = true;
   setLight();
 }
+
+
 
 function creating(task) {
   const task_el = document.createElement("div");
